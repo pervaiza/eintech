86f9c48478fa8716bc9da3cd595969322f4fa622
pervaiz akhtar <m.akhtar@hotmail.co.uk>
2021-08-03 15:36:32 +0100

    initial models

---
+++ eintech/eintech.api/Models/PersonDbContext.cs
@@ -0,0 +1,20 @@
+﻿using eintech.domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eintech.api.Models
+{
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
